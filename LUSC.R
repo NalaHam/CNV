54997eaf5c4e9b706add97d262c12d1a29cc72e1
Nala Hamilton <nalahamilton@Nalas-MacBook-Air.local>
2022-04-11 18:15:42 -0400

    stuff

---
+++ LUSC.R
@@ -0,0 +1,87 @@
+library(TCGAbiolinks)
+library(dplyr)
+
+#LUSC
+clin <- GDCquery_clinic("TCGA-LUSC", type = "clinical", save.csv = TRUE)
+sub_clin <- subset(clin, select = c("submitter_id", "ajcc_pathologic_stage", 
+                                    "gender", "age_at_index"))
+
+#Somatic Mutations:
+query1 <- GDCquery( project = "TCGA-LUSC", 
+                    data.category = "Simple Nucleotide Variation", 
+                    data.type = "Masked Somatic Mutation", legacy=F)
+
+GDCdownload(query1)
+
+muts <- GDCprepare(query1)
+
+muts_sub <- muts[muts$Variant_Classification != "Silent", ] #remove rows that are silent mutations
+
+muts_sub <- subset(muts_sub, select = c( "Hugo_Symbol", "Chromosome", "Start_Position", 
+                                         "End_Position", "Variant_Classification",
+                                         "Variant_Type", "Tumor_Sample_Barcode", "Matched_Norm_Sample_Barcode",
+                                         "Mutation_Status", "HGVSp"))
+
+
+submitter_id <- substr(muts_sub$Tumor_Sample_Barcode, 1, 12)
+
+muts_sub$submitter_id <- submitter_id
+
+mut_data <- merge(muts_sub, sub_clin, by = "submitter_id", all = T)
+
+names(mut_data)[1] <- 'sample_id'
+names(mut_data)[2] <- 'GeneSymbol'
+names(mut_data)[12] <- 'cancer_level'
+
+write.csv(mut_data,"LUSC_tsg.csv")
+
+
+#separate into stages and ages
+unique(clin[c("ajcc_pathologic_stage")])
+mut_data_1 <- filter(mut_data, cancer_level == "Stage I"| cancer_level == "Stage IA"
+                     | cancer_level == "Stage IB" | cancer_level == "Stage II" 
+                     | cancer_level == "Stage II"| cancer_level == "Stage IIA"
+                     | cancer_level == "Stage IIB",
+                     age_at_index <= 55, 
+                     na.rm = TRUE)
+
+#Mutations and Tumor Suppressors compared:
+tsg <- semi_join(mut_data_1, Human_TSGs, by = "GeneSymbol") #make new df where strings in column GeneSymbol match in df mut_data and df Human_TSGs
+
+#Get information for just X chromosome mutations:
+xchr_tsg <- filter(tsg, tsg$Chromosome == "chrX") #make new df for rows in Chromosome column = chrX 
+
+lusc_tsg <- unique(xchr_tsg[c("GeneSymbol")])
+
+#sums of male mutations
+lusc_tsg$male <- c(sum(xchr_tsg$GeneSymbol == "BTK" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "DMD" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "DDX3X" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "SRPX" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "RPS6KA6" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "XIST" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "FLNA" & xchr_tsg$gender == "male"), 
+                   sum(xchr_tsg$GeneSymbol == "BCORL1" & xchr_tsg$gender == "male"), 
+                   sum(xchr_tsg$GeneSymbol == "KDM6A" & xchr_tsg$gender == "male"), 
+                   sum(xchr_tsg$GeneSymbol == "GPC3" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "AMER1" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "PHF6" & xchr_tsg$gender == "male"),
+                   sum(xchr_tsg$GeneSymbol == "FOXO4" & xchr_tsg$gender == "male"))
+
+#sums of female mutations
+lusc_tsg$female <- c(sum(xchr_tsg$GeneSymbol == "BTK" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "DMD" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "DDX3X" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "SRPX" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "RPS6KA6" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "XIST" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "FLNA" & xchr_tsg$gender == "female"), 
+                     sum(xchr_tsg$GeneSymbol == "BCORL1" & xchr_tsg$gender == "female"), 
+                     sum(xchr_tsg$GeneSymbol == "KDM6A" & xchr_tsg$gender == "female"), 
+                     sum(xchr_tsg$GeneSymbol == "GPC3" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "AMER1" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "PHF6" & xchr_tsg$gender == "female"),
+                     sum(xchr_tsg$GeneSymbol == "FOXO4" & xchr_tsg$gender == "female"))
+
+#find difference btw males and females for each tumor suppressor
+lusc_tsg$lusc_bias <- lusc_tsg$male - lusc_tsg$female
